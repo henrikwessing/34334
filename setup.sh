b6d543b23d4b739a1824f05ed2c6f693d7998c8a
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2021-09-19 19:46:43 +0200

    Update setup.sh
    Klar til SNORT øvelse

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#/bin/bash
 exo-open --launch TerminalEmulator
 echo "Opretter laboratoriesetup i Virtuel Maskine til kursus 34334"
 echo "Laboratoriesetup er ikke klar endnu. Prøv igen senere"
+cd ~/
+sudo pip install gdown
+gdown --id 1WscKcNj1wNrviL81FZZgnai3NsHzzIle
+tar -vxf ids.tar.gz
+sudo docker load -i ids.tar
+rm ids.tar
+rm ids.tar.gz
+sudo docker run -i --net=host --name=snort 34334:ids &
