99bb36e0c033595463943681692cdfab98a6296f
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2021-09-19 19:48:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #/bin/bash
-exo-open --launch TerminalEmulator
-echo "Opretter laboratoriesetup i Virtuel Maskine til kursus 34334"
-echo "Laboratoriesetup er ikke klar endnu. Prøv igen senere"
 cd ~/
 sudo pip install gdown
 gdown --id 1WscKcNj1wNrviL81FZZgnai3NsHzzIle
