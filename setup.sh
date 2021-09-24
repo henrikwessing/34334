96c3b345d66f01e243c6841893335f8a0257ebd2
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2021-09-24 23:48:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
-exo-open --launch TerminalEmulator
-echo "Opretter laboratoriesetup i Virtuel Maskine til kursus 34334"
-echo "Laboratoriesetup er ikke klar endnu. Prøv igen senere"
+#/bin/bash
+cd ~/34334
+
+if [ -z "$(sudo docker images 34334:ids -q)"] 
+then 
+	sudo pip install gdown
+	gdown --id 1WscKcNj1wNrviL81FZZgnai3NsHzzIle
+	tar -vxf ids.tar.gz
+	sudo docker load -i ids.tar
+	rm ids.tar
+	rm ids.tar.gz
+fi 
+sudo apt-get install bridge-utils
+sudo python lab_webapp.py
