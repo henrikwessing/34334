fafe98e230805871bf87eea527f8848d86caa8ee
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2023-09-06 17:45:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@ cd ~/34334
 
 if [ -z "$(sudo docker images 34334:ids -q)"] 
 then 
-	sudo pip install gdown
-	gdown 1WscKcNj1wNrviL81FZZgnai3NsHzzIle
 	tar -vxf ids.tar.gz
-	sudo docker load -i ids.tar
+	sudo docker load -i ../snort/ids.tar
 	rm ids.tar
 	rm ids.tar.gz
 fi 
