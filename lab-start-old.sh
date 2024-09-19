b5a8cacbd395d45bf1b3eab109e3b24b92d2576f
Henrik Wessing <hewe@fotonik.dtu.dk>
2024-09-19 09:49:09 +0200

    Moved setup to lab-start to fit with VM lab-init

---
+++ lab-start-old.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+cd ~/34334
+
+if [ -z "$(sudo docker images 34334:ids -q)"] 
+then 
+	sudo pip install gdown
+	gdown --id 1WscKcNj1wNrviL81FZZgnai3NsHzzIle
+	tar -vxf ids.tar.gz
+	sudo docker load -i ids.tar
+	rm ids.tar
+	rm ids.tar.gz
+fi 
+sudo apt-get install bridge-utils
+sudo apt install iperf
+sudo apt install iproute2
+sudo apt install firefox-esr
+sudo python lab_webapp.py
--- lab-start.sh
+++ lab-start.sh
@@ -1,17 +1,11 @@
 #/bin/bash
 cd ~/34334
+sudo apt-get install iperf
 
-if [ -z "$(sudo docker images 34334:ids -q)"] 
+if [ -z "$(sudo docker images 34334:ids -q)" ] 
 then 
-	sudo pip install gdown
-	gdown --id 1WscKcNj1wNrviL81FZZgnai3NsHzzIle
-	tar -vxf ids.tar.gz
+	tar -vxf ../snort/ids.tar.gz
 	sudo docker load -i ids.tar
 	rm ids.tar
-	rm ids.tar.gz
 fi 
-sudo apt-get install bridge-utils
-sudo apt install iperf
-sudo apt install iproute2
-sudo apt install firefox-esr
-sudo python lab_webapp.py
+# sudo python lab_webapp.py
