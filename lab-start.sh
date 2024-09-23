52b6782f86b3c46cd0098036d546281ad68b8763
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2024-09-23 12:15:12 +0200

    Update lab-start.sh

--- lab-start.sh
+++ lab-start.sh
@@ -5,9 +5,10 @@ sudo apt-get install iperf
 if [ -z "$(sudo docker images 34334:ids -q)" ] 
 then 
 	wget https://files.cyberteknologi.dk/ids-$(uname -m).tar.gz -O ids.tar.gz
-	tar -vxf ids.tar.gz && rm ids.tar.gz 
+	gunzip ids.tar.gz 
 	sudo docker load -i ids.tar
 	rm ids.tar
+ 	rm ids.tar.gz
 fi
  
 sudo python lab_webapp.py
