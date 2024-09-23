ade116364fa52f7ad0d68031716a65d0a248c7d8
henrikwessing <72068231+henrikwessing@users.noreply.github.com>
2024-09-23 11:22:04 +0200

    Update lab-start.sh

--- lab-start.sh
+++ lab-start.sh
@@ -4,7 +4,7 @@ sudo apt-get install iperf
 
 if [ -z "$(sudo docker images 34334:ids -q)" ] 
 then 
-	wget wget https://files.cyberteknologi.dk/ids-$(uname -m).tar.gz -O ids.tar.gz
+	wget https://files.cyberteknologi.dk/ids-$(uname -m).tar.gz -O ids.tar.gz
 	tar -vxf ids.tar.gz && rm ids.tar.gz 
 	sudo docker load -i ids.tar
 	rm ids.tar
