59ac53bd4dc40476967dfc8ffd4aea35dacd3def
Henrik Wessing <hewe@fotonik.dtu.dk>
2024-09-26 13:44:08 +0200

    Changed service start nginx in containers

--- lab-start.sh
+++ lab-start.sh
@@ -5,7 +5,7 @@ sudo apt-get install iperf
 if [ -z "$(sudo docker images 34334:ids -q)" ] 
 then 
 	wget https://files.cyberteknologi.dk/ids-$(uname -m).tar.gz -O ids.tar.gz
-	tar -vxf ids.tar.gz && rm ids.tar.gz 
+	gunzip ids.tar.gz 
 	sudo docker load -i ids.tar
 	rm ids.tar
 fi
